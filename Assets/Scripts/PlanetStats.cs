8b06ab39c8daf394494846c4dec49d5321d3c35f
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-01-17 23:48:50 -0500

    feat: :begin: solar freaking system
    Signed-off-by: Diogenesoftoronto <diogenesoft@protonmail.com>

---
+++ Assets/Scripts/PlanetStats.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetStats : MonoBehaviour
+{
+    // Define the variables for the planet stats
+    struct PlanetInfo
+    {
+        public float planetRadius;
+        public float planetMass;
+        public float planetDistancefromSun;
+        public float planetVelocity;
+
+        public float planetSpinSpeed;
+        public string planetName;
+        public string planetType;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // 
+}
